29b6664120268590cf67a257f9f16f75108c6b3b
pinkxpink00 <kyperu07@gmail.com>
2023-10-05 01:19:36 +0300

    add automapping

---
+++ Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using System.Reflection;
+using System.Linq;
+
+namespace Notes.Application.Common.Mappings
+{
+    // Клас AssemblyMappingProfile реалізує профіль AutoMapper для автоматичного встановлення відображень з вказаної збірки.
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingsFromAssembly(assembly);
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            // Отримати всі видимі (експортовані) типи з вказаної збірки.
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                 .Any(i => i.IsGenericType &&
+                 i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                // Створити екземпляр типу.
+                var instance = Activator.CreateInstance(type);
+
+                // Отримати метод "Mapping" для налаштування відображення.
+                var methodInfo = type.GetMethod("Mapping");
+
+                // Викликати метод "Mapping" для встановлення відображення для поточного профілю.
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Notes.Application.Common.Mappings
+{
+    // Інтерфейс IMapWith<T> визначає метод для налаштування відображення між типами.
+    public interface IMapWith<T>
+    {
+        // Метод Mapping приймає об'єкт Profile з AutoMapper і встановлює відображення між типами T та поточним типом.
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
