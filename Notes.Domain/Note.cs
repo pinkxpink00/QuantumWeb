79cccf6863f0d97003711c657f37464214784894
pinkxpink00 <kyperu07@gmail.com>
2023-10-04 23:23:31 +0300

    add Domain

--- Notes.Domain/Note.cs
+++ Notes.Domain/Note.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Notes.Domain
 {
-    internal class Note
+    class Note
     {
+        public Guid Id {  get; set; }
+        public string Title {  get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime EditDate { get; set; }
     }
 }
