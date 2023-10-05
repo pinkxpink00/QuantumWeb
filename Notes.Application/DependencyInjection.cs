772d88300e99a8da6025f6c0b613e4180b6a48c7
pinkxpink00 <kyperu07@gmail.com>
2023-10-05 17:48:22 +0300

    add description

--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -6,10 +6,12 @@ namespace Notes.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplication(
-            this IServiceCollection services)
+        // Метод розширення для налаштування залежностей Application Layer
+        public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            // Додавання всіх обробників запитів та команд (MediatR)
             services.AddMediatR(Assembly.GetExecutingAssembly());
+
             return services;
         }
     }
--- Notes.Persistence/DependecyInjection.cs
+++ Notes.Persistence/DependecyInjection.cs
@@ -8,16 +8,23 @@ namespace Notes.Persistence
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddPersistence(this IServiceCollection
-            services, IConfiguration configuration)
+        // Метод розширення для налаштування залежностей Persistence Layer
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
+            // Отримання рядка підключення до бази даних з конфігурації
             var connectionString = configuration["DbConnection"];
+
+            // Додавання контексту бази даних (NotesDbContext) у контейнер залежностей
             services.AddDbContext<NotesDbContext>(options =>
             {
+                // Використання Npgsql як провайдера для PostgreSQL та передача рядка підключення
                 options.UseNpgsql(connectionString);
             });
+
+            // Додавання служби INotesDbContext, яка буде реалізована контекстом бази даних
             services.AddScoped<INotesDbContext>(provider =>
                 provider.GetService<NotesDbContext>());
+
             return services;
         }
     }
